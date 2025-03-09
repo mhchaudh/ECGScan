134bc7e4cfde51911da97cd29106dba619dcb88e
MohamedAl-Nassirat <malnassi@ualberta.ca>
2025-03-09 14:31:43 -0600

    mock comparison completed

--- backend/ECGenius/api/__init__.py
+++ backend/ECGenius/api/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 from ECGenius.api.image_routes import image_bp
-
+from ECGenius.api.ecg_routes import ecg_bp
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={r"/api/*": {"origins": "*"}})
     app.register_blueprint(image_bp, url_prefix='/api/image')
+    app.register_blueprint(ecg_bp, url_prefix='/api/ecg')
     return app
